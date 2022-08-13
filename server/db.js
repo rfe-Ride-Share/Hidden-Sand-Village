b605d4ff532244ea7c5535415f22d51cbaf9f23f
Juhyun Suh <shipalnomoo@gmail.com>
2022-08-13 16:32:58 -0500

    Add endpoints for server

--- server/db.js
+++ server/db.js
@@ -1,9 +1,61 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
+const { Schema } = mongoose;
 
-main()
-  .then(() => console.log("connected to MongoDB..."))
+mongoose
+  .connect('mongodb://localhost:27017/sandy')
+  .then(() => console.log('connected to MongoDB...'))
   .catch((err) => console.log(err));
 
-async function main() {
-  await mongoose.connect("mongodb://localhost:27017/test");
+const tripSchema = new Schema({
+  date: { type: Date, default: Date.now },
+  depart_time: String,
+  departure: { type: String, required: true },
+  destination: { type: String, required: true },
+  driver: { type: String, required: true },
+  passengers: [
+    { name: { departure: String, destination: String, status: String } },
+  ],
+  price: Number,
+  duration: String,
+  status: String, // pending/done/cancelled/upcoming/full/
+});
+
+const Trip = mongoose.model('Trip', tripSchema);
+
+function createTrip(data) {
+  Trip.create(data, function (err) {
+    if (err) return handleError(err);
+  });
 }
+
+const userSchema = new Schema({
+  user_id: { type: String, required: true },
+  user_photo: String,
+  first_name: { type: String, required: true },
+  last_name: { type: String, required: true },
+  reviews: [{ stars: Number, review_text: String }],
+  bio: String,
+  current_trip: Schema.Types.ObjectId,
+  past_trips: [Schema.Types.ObjectId],
+  future_trips: [Schema.Types.ObjectId],
+});
+
+const user = mongoose.model('User', userSchema);
+
+function createUser(data) {
+  user.create(data, function (err) {
+    if (err) return handleError(err);
+  });
+}
+
+function getProfileData(id) {}
+
+function postProfileData(id) {}
+// user
+//  picture
+//  name
+//  reviews
+//  bio
+// current past and future trips
+
+// chat
--- server/index.js
+++ server/index.js
@@ -9,5 +9,27 @@ app.use(express.static(path.join(__dirname, '../client/dist')));
 
 const PORT = 3000;
 
+// trips
+app.get('/trips', (req, res) => {
+  res.send('trips data');
+});
+
+app.post('/trips', (req, res) => {
+  res.send('trips post data');
+});
+app.put('/trips', (req, res) => {
+  res.send('trips update data');
+});
+
+// user
+app.get('/user', (req, res) => {
+  res.send('user data');
+});
+
+app.get('*', (req, res) => {
+  res.status(404).send('Page does not exist...');
+});
+
+// chat??
 app.listen(PORT);
 console.log('Server listening at http://localhost:3000');

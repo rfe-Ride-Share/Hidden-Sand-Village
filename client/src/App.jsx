2fc59942a8d718fe9cf00849e38dc838cf8106ad
Anthony Cella <anthony.cella242@gmail.com>
2022-08-15 09:56:24 -0400

    Got variable ride information integrated into the rider view. I think that just about does it for the rider view

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -8,15 +8,17 @@ import SearchView from './components/search-view/search-view';
 import RiderTripView from './components/trip-view/rider-trip-view/rider-trip-view';
 
 function App() {
+  const tripInfo = {
+    date: '4:00 PM August 27th, 2022',
+    startPoint: '20 W 34th St., New York, NY 10001',
+    endPoint: '3701 Osceola Pkwy, Bay Lake, FL 32830',
+    totalCost: '$250',
+    riderCostLow: 25,
+    riderCostHigh: 125,
+  }
+
   return (
-    <div>
-      <div>This is a very important message!!</div>
-      <div>Put your component here when you want to test it out.</div>
-      <div>Please save these divs in a comment below when testing.</div>
-      <div>So that you are able to restore this component to exactly</div>
-      <div>the way it was to avoid merge conflicts.</div>
-      <div>This message is coming to you from App.jsx</div>
-    </div>
+    <RiderTripView tripInfo={tripInfo} />
   )
 }
 

28c95e9245f4e63f4c7e9b10e8af727750d3cf53
Juhyun Suh <shipalnomoo@gmail.com>
2022-08-15 15:18:15 -0500

    Add index

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,8 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { Auth0Provider } from '@auth0/auth0-react';
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <App />,
+  <Auth0Provider
+    domain="YOUR_AUTH0_DOMAIN"
+    clientId="YOUR_AUTH0_CLIENT_ID"
+    redirectUri={window.location.origin}
+  >
+    <App />
+  </Auth0Provider>
 );

c5fbc0db2dcc50cb0923570d53f3f662cc6462a0
Anthony Cella <anthony.cella242@gmail.com>
2022-08-15 08:51:40 -0400

    Put the app.jsx back where it is supposed to be

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -8,15 +8,16 @@ import SearchView from './components/search-view/search-view';
 import RiderTripView from './components/trip-view/rider-trip-view/rider-trip-view';
 
 function App() {
-  return <RiderTripView status='confirmed' />;
+  return (
+    <div>
+      <div>This is a very important message!!</div>
+      <div>Put your component here when you want to test it out.</div>
+      <div>Please save these divs in a comment below when testing.</div>
+      <div>So that you are able to restore this component to exactly</div>
+      <div>the way it was to avoid merge conflicts.</div>
+      <div>This message is coming to you from App.jsx</div>
+    </div>;
+  )
 }
 
 export default App;
-<div>
-  <div>This is a very important message!!</div>
-  <div>Put your component here when you want to test it out.</div>
-  <div>Please save these divs in a comment below when testing.</div>
-  <div>So that you are able to restore this component to exactly</div>
-  <div>the way it was to avoid merge conflicts.</div>
-  <div>This message is coming to you from App.jsx</div>
-</div>;

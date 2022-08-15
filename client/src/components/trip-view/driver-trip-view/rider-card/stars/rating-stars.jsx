9910af8a383dbf08468886d105ab8131b26b31f0
Anthony Cella <anthony.cella242@gmail.com>
2022-08-15 14:18:48 -0400

    got the rating stars to be the yellow color

--- client/src/components/trip-view/driver-trip-view/rider-card/stars/rating-stars.jsx
+++ client/src/components/trip-view/driver-trip-view/rider-card/stars/rating-stars.jsx
@@ -9,11 +9,11 @@ function getStarsFromNumber(number) {
   let icons = [];
   for (let currentIteration = 0; currentIteration < 5; currentIteration++) {
     if (number >= 0.75) {
-      icons.push(<StarIcon />);
+      icons.push(<StarIcon sx={{ color: '#F5B935' }} />);
     } else if (number >= 0.5) {
-      icons.push(<StarHalfIcon />);
+      icons.push(<StarHalfIcon sx={{ color: '#F5B935' }} />);
     } else {
-      icons.push(<StarOutlineIcon />);
+      icons.push(<StarOutlineIcon sx={{ color: '#F5B935' }} />);
     }
 
     number--;

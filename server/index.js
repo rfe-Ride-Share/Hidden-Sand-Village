9863f87db91f624ced5babc98ba1690fd5efddbd
Juhyun Suh <shipalnomoo@gmail.com>
2022-08-17 10:45:48 -0500

    Add some authenticaitons

--- server/index.js
+++ server/index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const path = require('path');
 
 const db = require('./db');
@@ -6,7 +7,8 @@ const db = require('./db');
 const app = express();
 
 app.use(express.json());
-
+// app.use(bodyParser.json());
+app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, '../client/dist')));
 
 const PORT = 3000;
@@ -41,25 +43,37 @@ app.delete('/tripp/:id', (req, res) => {
 });
 
 // user
-app.get('/user/:id', (req, res) => {
-  db.getProfileData(req.params.id)
-    .then((response) => res.send(response))
-    .catch(() => res.send('User not found ❌'));
+app.get('/userr/', (req, res) => {
+  console.log('get user', req.query);
+  db.getProfileData(req.query)
+    .then((response) => {
+      console.log('response is', response);
+      if (!response) {
+        res.status(400).send('User not found ❌');
+      } else {
+        res.send(response);
+      }
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send(err);
+    });
 });
 
-app.post('/user/', (req, res) => {
+app.post('/userr', (req, res) => {
+  console.log('creating user', req.body);
   db.createUser(req.body)
     .then(() => res.status(201).send('User created ✅'))
     .catch((err) => res.status(500).send('Could not create or update user ❌'));
 });
 
-app.put('/user/:id', (req, res) => {
+app.put('/userr/:id', (req, res) => {
   db.findOneAndUpdateUser(req.params.id, req.body)
     .then(() => res.status(201).send('User updated ✅'))
     .catch((err) => res.status(500).send('Could not create or update user ❌'));
 });
 
-app.delete('/user/:id', (req, res) => {
+app.delete('/userr/:id', (req, res) => {
   db.deleteUser(req.params.id)
     .then((data) => {
       data.deletedCount === 1
@@ -78,7 +92,7 @@ app.listen(PORT);
 console.log('Server listening at http://localhost:3000');
 
 const testUser = {
-  user_id: 1000,
+  email: 'ss@n.com',
   user_photo: 'fakeUrl.picture',
   first_name: 'IAN',
   last_name: 'ZUBER',
@@ -91,19 +105,26 @@ const testUser = {
 // possibly change driver and passengers based on userid
 
 const sampleTrip = {
+  title: 'Alien hunting trip',
+  description:
+    'Aliens make too much environment issues and need to be restrained',
   date: '1660576677204',
   depart_time: '1660576677205',
   departure: 'home',
   depart_coord: { lat: '123', lng: '321' },
   destination: 'home away from home',
   dest_coord: { lat: '123', lng: '321' },
-  driver: 'Carl Poole', // driver email
-  notes: 'Looking for an alien.',
+  driver: 'CarlPoole@mail.com', // driver email
   distance: 1,
   distance_str: 'one mile',
   passengers: [
     {
-      Rider: { departure: 'place1', destination: 'place2', status: 'upcoming' },
+      Rider1: {
+        departure: 'place1',
+        destination: 'place2',
+        status: 'upcoming',
+        email: 'forThe@environment.com',
+      },
     },
   ],
   passenger_capacity: 3,

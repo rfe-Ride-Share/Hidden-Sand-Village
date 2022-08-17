646d4f2f863bf6212414754ced5841e6ad62af72
Juhyun Suh <shipalnomoo@gmail.com>
2022-08-17 16:01:13 -0500

    Capitalizing filenames

--- client/src/components/chat/conversation.jsx
+++ client/src/components/chat/conversation.jsx
@@ -1,25 +1,15 @@
 import React from 'react';
 import styled from 'styled-components';
-import Message from './message.jsx'
+import Message from './Message.jsx';
 
 function Conversation() {
-  return (
-    <Scroll className="conversation">
-
-
-    </Scroll>
-
-  );
+  return <Scroll className="conversation"></Scroll>;
 }
 
-
 const Scroll = styled.div`
-
-
-overflow:scroll;
-height:700px;
-width:400px;
-
-`
+  overflow: scroll;
+  height: 700px;
+  width: 400px;
+`;
 
 export default Conversation;

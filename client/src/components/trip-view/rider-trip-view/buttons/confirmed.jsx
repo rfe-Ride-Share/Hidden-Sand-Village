62823e1469d5d6b36a66371ae0631412e57cb7b6
Anthony Cella <anthony.cella242@gmail.com>
2022-08-17 11:39:23 -0400

    Created the pseudocode for the function that will update the trip

--- client/src/components/trip-view/rider-trip-view/buttons/confirmed.jsx
+++ client/src/components/trip-view/rider-trip-view/buttons/confirmed.jsx
@@ -4,7 +4,17 @@ import axios from 'axios';
 import Stack from '@mui/material/Stack';
 import Button from '@mui/material/Button';
 
-function Confirmed() {
+function addPendingUserToTrip() {
+  // get user email
+  // grab user data by email
+
+  // build rider object
+  // add rider to the end of passengers of trip
+
+  // update trip with the new rider
+}
+
+function Confirmed({ tripInfo }) {
   return (
       <Button
         variant="contained"

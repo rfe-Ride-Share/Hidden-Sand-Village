fa28cda0ad377b4814255546a20b2ce98ba99abf
Anthony Cella <anthony.cella242@gmail.com>
2022-08-17 00:17:48 -0400

    Payment route complete

--- client/src/components/payment/payment.jsx
+++ client/src/components/payment/payment.jsx
@@ -1,11 +1,21 @@
 import React from 'react';
 
 import PayPalButton from './paypal-button';
+import PaymentCard from './payment-card';
 
 function Payment() {
+  const sampleTrip = {
+    date: '4:00 PM 6/2/2023',
+    startPoint: 'New York, NY',
+    endPoint: 'Orlando, FL',
+    riderCost: 50,
+  }
   return (
-    <PayPalButton />
-  )
+    <div>
+      <PaymentCard tripInfo={sampleTrip} />
+      <PayPalButton />
+    </div>
+  );
 }
 
-export default PayPalButton;
+export default Payment;

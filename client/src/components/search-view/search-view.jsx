509eb5a1905957290c02e0bbbc1ebaa14fd4a720
Michael Schoenecker <michael.schoenecker@gmail.com>
2022-08-17 12:39:46 -0400

    prep commit

--- client/src/components/search-view/search-view.jsx
+++ client/src/components/search-view/search-view.jsx
@@ -6,72 +6,51 @@ import {
   Marker,
   DirectionsRenderer,
 } from '@react-google-maps/api';
+import axios from 'axios';
 
 import Box from '@mui/material/Box';
 import Stack from '@mui/material/Stack';
 import Container from '@mui/material/Container';
 
 import SearchBar from './searchBar';
-import MapDirections from './mapDirections';
+// import MapDirections from './MapDirections';
 import RiderCard from '../trip-view/rider-trip-view/rider-card';
 
 import getDistance from './helpers/getDistance';
-
-// const library = ['places'];
 
 export default function SearchView() {
   //Longitude & latitude setters
   const [startPos, setStartPos] = useState({ lat: 0, lng: 0 });
   const [destPos, setDestPos] = useState({ lat: 0, lng: 0 });
-  const [distance, setDistance] = useState(0);
+  const [trips, setTrips] = useState([]);
 
-  const [getDirs, setDirs] = useState(false);
+  // useEffect(() => {
+  //   axios
+  //     .get('/trips')
+  //     .then((response) => {
+  //       console.log({ response });
+  //       setTrips(response);
+  //     })
+  //     .catch((err) => {
+  //       console.log(err);
+  //     });
+  // }, []);
 
-  const fetchDirections = (start, end) => {
-    // if (!isLoaded) {
-    //   return;
-    // }
-    const service = new google.maps.DirectionsService();
-    service.route(
-      {
-        origin: start,
-        destination: end,
-        travelMode: google.maps.TravelMode.DRIVING,
-      },
-      (result, status) => {
-        if (status === 'OK' && result) {
-          setDirs(result);
-          let miles = result.routes[0].legs[0].distance.value / 1609.34;
-          let seconds = result.routes[0].legs[0].duration.value;
-          let startAddress = result.routes[0].legs[0].start_address;
-          let endAddress = result.routes[0].legs[0].end_address;
-          let centsPerMile = 62.5;
-          console.log('🚙', Math.round(miles));
-          console.log('⏰', seconds);
-          console.log('🚪', startAddress);
-          console.log('📍', endAddress);
-          console.log('💰', `$${((miles * centsPerMile) / 100).toFixed(2)}`);
-          console.log(result);
-        }
-      }
-    );
-  };
-
-  useEffect(() => {
-    console.log(startPos, destPos);
-    fetchDirections(startPos, destPos);
-    //Sort trips by distance to input locations assuming tripsResults is array of trip objects
-    let tripsCopy = tripResults.slice();
-    tripsCopy.sort((a, b) => {
-      if (totalDistance(a) < totalDistance(b)) {
-        return 1;
-      }
-      if (totalDistance(a) > totalDistance(b)) {
-        return -1;
-      }
-      return 0;
-    });
-  }, [startPos, destPos]);
+  // useEffect(() => {
+  //   console.log(startPos, destPos);
+  //   fetchDirections(startPos, destPos);
+  //   //Sort trips by distance to input locations assuming tripsResults is array of trip objects
+  //   let tripsCopy = tripResults.slice();
+  //   tripsCopy.sort((a, b) => {
+  //     if (totalDistance(a) < totalDistance(b)) {
+  //       return 1;
+  //     }
+  //     if (totalDistance(a) > totalDistance(b)) {
+  //       return -1;
+  //     }
+  //     return 0;
+  //   });
+  // }, [startPos, destPos]);
 
   //function to calculate total distance between searched positions and trip positions card values are placeholders use
 
@@ -116,49 +95,12 @@ export default function SearchView() {
           alignItems: 'center',
         }}
       >
-        <RiderCard />
-        <RiderCard />
-        <RiderCard />
+        <br> </br>
         {/* <MapDirections startPos={startPos} destPos={destPos} /> */}
+        <RiderCard />
+        <RiderCard />
+        <RiderCard />
       </Box>
     </Container>
   );
 }
-// const testUser = {
-//   user_id: 1000,
-//   user_photo: 'fakeUrl.picture',
-//   first_name: 'IAN',
-//   last_name: 'ZUBER',
-//   reviews: [
-//     { stars: 1, review_text: 'So wack' },
-//     { stars: 5, review_text: 'wonderful' },
-//   ],
-//   bio: 'Just a small town girl, livin in a lonely world',
-// };
-
-// Trip Data
-// sampleTrip = {
-//   date: '1660576677204',
-//   depart_time: '1660576677205',
-//   origin_address: '89 E 42nd St, New York, NY 10017, USA',
-//   origin_position: { lat: 35.3444, lng: 35.344455566 },
-//   destination_address: '349-399 US-11, Syracuse, NY 13202, USA',
-//   destination_position: { lat: 40.7527262, lng: -73.9772294 },
-//   driver: 'Carl Poole',
-//   driver_email: 'carl.poole@gmail.com',
-//   details: 'This is going to be the best trip EVER!!!!', //from notes
-//   distance: '3.9 miles',
-//   total_meters: 7410,
-//   passengers: [
-//     {
-//       Rider: { departure: 'place1', destination: 'place2', status: 'upcoming' },
-//     },
-//   ],
-//   totalPassengers: 5,
-//   currentPassengers: 1,
-//   price: '2.38',
-//   duration: 'one hour',
-//   duration_seconds: 3600,
-//   status: 'upcoming', // pending/done/cancelled/upcoming/full/ //is this driver status? do we need upcoming and completed?
-//   //can the database automatically update completed and upcoming based on time, like one day after trip for completed
-// };

cd9e0189287fae7e246748fb3421fa10112dc414
Juhyun Suh <shipalnomoo@gmail.com>
2022-08-19 07:27:19 -0500

    Socket

--- client/src/components/chat/conversation.jsx
+++ client/src/components/chat/conversation.jsx
@@ -1,19 +1,15 @@
 import React, { useState, useEffect, useRef } from 'react';
 import { useAuth0 } from '@auth0/auth0-react';
-import axios from 'axios'
+import axios from 'axios';
 
 import Button from '@mui/material/Button';
 import TextField from '@mui/material/TextField';
 
-
-
-
 const { io } = require('socket.io-client');
 // const socket = io.connect('http://localhost:3001');
 
-
 function Conversation(props) {
-  const [newMessage, setNewMessage] = useState("");
+  const [newMessage, setNewMessage] = useState('');
   const [onlineUsers, setOnlineUsers] = useState([]);
   const [message, setMessage] = useState('');
   const [messageReceived, setmessageReceived] = useState('');
@@ -26,8 +22,9 @@ function Conversation(props) {
   const currentChat = props.currentChat;
 
   useEffect(() => {
-    socket.current = io("ws://localhost:8900");
-    socket.current.on("getMessage", (data) => {
+    socket.current = io('ws://localhost:8900');
+    socket.current.on('getMessage', (data) => {
+      console.log('message receive', data);
       setArrivalMessage({
         sender: data.senderId,
         text: data.text,
@@ -42,18 +39,14 @@ function Conversation(props) {
       props.setMessages((prev) => [...prev, arrivalMessage]);
   }, [arrivalMessage, currentChat]);
 
-
-
   const sendMessage = (event) => {
     event.preventDefault();
 
     const messageObj = {
       sender: props.currentUser._id,
       text: message,
-      conversationId:  props.currentChat._id,
-    }
-
-
+      conversationId: props.currentChat._id,
+    };
 
     // socket.emit('send_message', {
     //   text: message,
@@ -61,14 +54,15 @@ function Conversation(props) {
     //   conversationId: props.currentChat._id,
     // });
 
-
-
-    axios.post('/messages', messageObj).then((res) => {
-      props.setMessages(props.messages.concat(res.data))
-      console.log(res)
-    })
+    axios
+      .post('/messages', messageObj)
+      .then((res) => {
+        props.setMessages(props.messages.concat(res.data));
+        console.log(res);
+      })
+      .catch((err) => console.error(err));
     setMessage();
-     event.target.reset();
+    event.target.reset();
   };
   // useEffect(() => {
   //   socket.on('receive_message', (data) => {
@@ -89,7 +83,9 @@ function Conversation(props) {
             setMessage(e.target.value);
           }}
         ></TextField>
-        <Button type="submit" variant="outlined">Send</Button>
+        <Button type="submit" variant="outlined">
+          Send
+        </Button>
       </form>
     </div>
   );
